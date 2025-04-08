6064f8381f9f0214f9f65f9553486ac91b2ad5d9
Everyweb <58446990+everywebco@users.noreply.github.com>
2025-04-08 10:54:36 +0200

    Add testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import IndexSectionLogoClouds3 from './components/logo-clouds/IndexSectionLogoCl
 import IndexSectionFeatures4 from './components/features/IndexSectionFeatures4';
 import IndexSectionCallToAction5 from './components/call-to-action/IndexSectionCallToAction5';
 import IndexSectionFooters7 from './components/footers/IndexSectionFooters7';
+import TestimonialsSectionTestimonials2 from './components/testimonials/TestimonialsSectionTestimonials2';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <IndexSectionLogoClouds3 />
       <IndexSectionFeatures4 />
       <IndexSectionCallToAction5 />
+      <TestimonialsSectionTestimonials2 />
       <IndexSectionFooters7 />
     </>
   );
